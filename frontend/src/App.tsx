af8615b804586c348c3bbfd69c18d5c9ffba2a30
imad-khaya <96892911+imad-khaya@users.noreply.github.com>
2024-12-29 16:54:02 +0100

    feat: added backend and installed some node packages and dockerized the backend btw

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import Navbar from './components/NavBar';
-import Footer from './components/Footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Content from './components/Content';
-import imagePath from './assets/logotu.png';
+
+import ProposalList from './components/ProposalList';
 
 
 
 function App() {
   return (
     <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-      <Navbar brandName="Hochschulwahl" imageScrPath={imagePath} />
-      <Content />
-      <Footer />
+      <ProposalList></ProposalList>
     </div>
   );
 }
