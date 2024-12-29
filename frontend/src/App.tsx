8bd07fe252f464c89782f2b96d2279431ea49cf0
imad-khaya <96892911+imad-khaya@users.noreply.github.com>
2024-12-29 16:54:57 +0100

    fix: App.tsx now shows original home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Navbar from './components/NavBar';
+import Footer from './components/Footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
-import ProposalList from './components/ProposalList';
+import Content from './components/Content';
+import imagePath from './assets/logotu.png';
 
 
 
 function App() {
   return (
     <div className="d-flex flex-column" style={{ minHeight: '100vh' }}>
-      <ProposalList></ProposalList>
+      <Navbar brandName="Hochschulwahl" imageScrPath={imagePath} />
+      <Content />
+      <Footer />
     </div>
   );
 }
