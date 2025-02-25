610382759d131a3bb9f390cc32f9dafc2bded5e0
Sirine <sirinezrouga@gmail.com>
2025-02-25 16:00:58 +0100

    email message updated

--- backend/forms/views.py
+++ backend/forms/views.py
@@ -36,7 +36,7 @@ def send_emails(request):
                     # Send an email to the candidate
                     send_mail(
                         subject='Hochschulwahlen –Einverständniserklärung zur Teilnahme an Vorschlagsliste erforderlich!',  # Email subject
-                        message=f"""Hallo / Guten Tag {first_name} {last_name},
+                        message=f"""Guten Tag {first_name} {last_name},
 
 Sie wurden für die Hochschulwahlen im Sommersemester {semesterYear}
 von der Vertrauensperson {trusteePerson} der Vorschlagsliste {password} 
